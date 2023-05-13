236d8019df0b8dabd7b9d5feee199f29e50055be
venkat <venkat@LAPTOP-SP0IHUO3>
2023-05-13 19:15:22 +0530

    added sys

--- src/main/java/com/java/SpringBootRestfulApplication.java
+++ src/main/java/com/java/SpringBootRestfulApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestfulApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Restful service");
 		SpringApplication.run(SpringBootRestfulApplication.class, args);
 	}
 
